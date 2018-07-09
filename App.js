937de7f074e110aa60539d16f688cce6aec2ff95
Jessah Duran <jessah@modernmachin.es>
2018-07-09 16:01:48 +0800

    Refactored UI styling

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
+import {
+  Image,
+  StyleSheet,
+  Text,
   TextInput,
-  TouchableOpacity
-  View 
+  TouchableOpacity,
+  View
 } from 'react-native';
+import { responsiveHeight } from 'react-native-responsive-dimensions';
+
+const image = require('./logo.png');
 
 export default class App extends React.Component {
   constructor(props) {
@@ -29,43 +33,141 @@ export default class App extends React.Component {
     this.setState({ password })
   }
 
+  handleSigninButtonPressed() {
+    //handle signin
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <View>
-          <Text>Email</Text>
-          <TextInput
-              label = "Email"
-              keyboardType = "email-address"
-              placeholder = "Input email address"
-              value = {this.state.email}
-              onChangeText={(email)=>{this.handleOnChangeEmail(email)}}
-          />
+        <Image
+          style={styles.image}
+          source={image}
+        />
+        <View style={styles.form}>
+          <View style={styles.card}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                  label = "Email"
+                  keyboardType = "email-address"
+                  placeholder = "Input email address"
+                  placeholderStyle = {styles.placeholder}
+                  value = {this.state.email}
+                  style = {styles.input}
+                  onChangeText={(email)=>{this.handleOnChangeEmail(email)}}
+              />
+            </View>
+            <Text style={styles.errorText}>
+              Error
+            </Text>
+          </View>
+
+          <View style={styles.card}>
+            <Text style={styles.label}>Password</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                label = "Password"
+                placeholder = "Input password"
+                placeholderStyle = {styles.placeholder}
+                value = {this.state.password}
+                style = {styles.input}
+                onChangeText={(password)=>{this.handleOnChangePassword(password)}}
+                secureTextEntry
+              />
+            </View>
+            <Text style={styles.errorText}>
+              Error
+            </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={this.handleSigninButtonPressed}>
+              <Text style={styles.buttonText}>
+                Sign In
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View>
-          <Text>Password</Text>
-          <TextInput
-            label="Password"
-            placeholder = "Input password"
-            value = {this.state.value}
-            onChangeText = {(password) => {this.handleOnChangePassword(password)}}
-            secureTextEntry
-          />
-        </View>
-        <TouchableOpacity onPress={this.handleSigninButtonPressed}>
-          <Text>Sign In</Text>
-        </TouchableOpacity>
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#7c57bb',
+    borderRadius: 5,
+    height: 30,
+  },
+  buttonContainer: {
+    height: 50,
+    marginVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    alignSelf: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '800',
+    paddingVertical: 12
+  },
+  card: {
+    backgroundColor: 'transparent',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    position: 'relative',
+    height: 100,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginVertical: 15,
+    marginHorizontal: 15,
   },
+  errorText: {
+    color: 'red',
+    fontStyle: 'italic',
+    paddingTop: 3
+  },
+  form: {
+    alignSelf: 'flex-end',
+    width: '100%',
+    paddingTop: 50
+  },
+  image: {
+    width: '100%',
+    height: responsiveHeight(30),
+    marginVertical: 5,
+  },
+  input: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 20,
+    paddingVertical: 10,
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
+    borderWidth: 1.5,
+    borderColor: '#7c57bb',
+    borderRadius: 5,
+    height: 30,
+  },
+  label: {
+    color: '#000',
+    fontSize: 20,
+    paddingVertical: 3,
+    fontWeight: '600'
+  },
+  placeholder: {
+    fontStyle: 'italic'
+  }
 });
